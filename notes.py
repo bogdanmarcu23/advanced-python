49174d7139b79730b0b165d426bd7e3372a6c18e
bogdanmarcu23 <150248007+bogdanmarcu23@users.noreply.github.com>
2024-03-07 12:16:47 +0200

    Update notes.py

--- notes.py
+++ notes.py
@@ -6,6 +6,6 @@ z = "1"
 x == y #True
 y == int(z) #True
 
-#default values
-"""number != 0""" equivalent to """number"""
+#default values for booleans: 0 for False, 1 is True
+"""number != 0""" equivalent to """number""" #because 
 """number % 2 == 1""" equivalent to """number % 2"""
