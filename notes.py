0417a9e69bd77ebe6188284a42024af8b0e34663
bogdanmarcu23 <150248007+bogdanmarcu23@users.noreply.github.com>
2024-03-07 12:35:13 +0200

    Update notes.py

--- notes.py
+++ notes.py
@@ -7,5 +7,7 @@ x == y #True
 y == int(z) #True
 
 #default values for booleans: 0 for False, 1 is True
-"""number != 0""" equivalent to """number""" #because 
+"""number != 0""" equivalent to """number""" #because 1 is True
 """number % 2 == 1""" equivalent to """number % 2"""
+
+"""range(100)""": starts from 0 .. 99. If no first number, the inital position is 0.
